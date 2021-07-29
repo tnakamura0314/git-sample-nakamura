6d2a748807529869e7c71e192ae8375abe8aa443
tnakamura0314 <reggae.a0215@gmail.com>
2021-07-29 11:41:15 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleNakamuraApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleNakamuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleNakamuraApplication.class, args);
+	}
+
+}
